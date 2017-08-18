12fc841b50f324c6b17b86e9580d77bd9ea5f363
Robert Gevorgyan <robert1999.g@gmail.com>
2017-08-18 22:26:46 +0400

    Add recursive functions, simple algos, if else

--- first_steps.ml
+++ first_steps.ml
@@ -7,3 +7,14 @@ let isVowel1 c =
   c = 'a' || c = 'e' || c  = 'i' || c = 'o' || c = 'u'
 let isSumTen a b =
   a + b = 10
+(* To define a recursive function, you need to specify the keyword rec *)
+let rec factorial1 x =
+  if x = 1 then 1 else x * factorial1 (x - 1)
+let rec gcd1 a b =
+  if b = 0 then a else gcd1 b (a mod b)
+(* Nested  if else construction: Not the best example though :) *)
+let rec power1 x n =
+  if n = 0 then 1 else
+    if n = 1 then x else
+      x * power1 x (n - 1)
+let isConsonant c = not (isVowel1 c)
