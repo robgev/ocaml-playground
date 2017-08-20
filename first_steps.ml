0bed7592549c41695422496b50c1bc1a07ade226
Robert Gevorgyan <robert1999.g@gmail.com>
2017-08-20 11:57:26 +0400

    Add insertion sort

--- first_steps.ml
+++ first_steps.ml
@@ -177,4 +177,19 @@ let rec make_set l =
   match l with
     [] -> []
   | h::t -> if member h t then make_set t else h :: make_set t
+
+(* Insertion sort. Understanding how to break the problem into small pieces using FP logic *)
+let rec insert x l =
+  match l with
+    [] -> [x]
+  | h::t ->
+     if x <= h
+     then x :: h :: t
+     else h :: insert x t
+
+let rec sort l =
+  match l with
+    [] -> []
+  | h::t -> insert h (sort t)
+       
   
